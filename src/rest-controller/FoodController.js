851dcc850a662ddcd48b03960d3f6d9b481d2201
Associate <Associate@roifmrwindt>
2023-10-11 11:39:23 +0530

    testing working))

--- src/rest-controller/FoodController.js
+++ src/rest-controller/FoodController.js
@@ -25,15 +25,21 @@ class FoodController {
     handleteamrequest(req, res) {
         res.send({
             "team": "Food",
-            "membersNames": ["Udupa","Aryan"]
+            "membersNames": ["Udupa", "Aryan"]
         })
     }
     getAllFoods(req, res) {
         const location = req.params.location;
         try {
             const foods = this.FoodDao.getAllFoodDao(location);
-            console.log("Size of the foods", foods.length);
-            res.json(foods);
+            if (foods != null) {
+                console.log("Size of the foods", foods.length);
+                res.json(foods);
+            }
+            else {
+                res.status(400).json({ error: "Bad request" });
+            }
+
         }
         catch (err) {
             console.error(`error on GET foods: ${err}`);
@@ -48,35 +54,45 @@ class FoodController {
         console.log(queryParams);
         console.log('got into foods');
         const foods = this.FoodDao.getAllFoodDao(location);
-
+        
         if (Object.keys(queryParams).length === 0) {
             console.log("No query parameters provided");
             res.json(foods);
         } else {
+            let check = 0;
+            if (Object.keys(queryParams).length == 4) {
+                if (queryParams.minprice && queryParams.maxprice && queryParams.brand && queryParams.rating) {
+                    check = 1;
+                }
+            }
             try {
                 let result = foods;
 
-                if (queryParams.minprice) {
+                if (queryParams.minprice && check == 1) {
                     const minPrice = parseFloat(queryParams.minprice);
+
                     result = result.filter(food => food.price >= minPrice);
                 }
 
-                if (queryParams.maxprice) {
+                if (queryParams.maxprice && check == 1) {
                     const maxPrice = parseFloat(queryParams.maxprice);
                     result = result.filter(food => food.price <= maxPrice);
                 }
 
-                if (queryParams.rating) {
+                if (queryParams.rating && check == 1) {
                     const minRating = parseFloat(queryParams.rating);
                     result = result.filter(food => food.rating >= minRating);
                 }
 
-                if (queryParams.brand) {
+                if (queryParams.brand && check == 1) {
                     const brand = queryParams.brand;
                     result = result.filter(food => food.brand.toLowerCase() === brand.toLowerCase());
                 }
-
-                res.json(result);
+                if (check)
+                    res.json(result);
+                else{
+                    res.status(400).json({error:"Bad request"})
+                }
             } catch (err) {
                 console.log("Caught exception.");
                 res.status(500).send('Something broke!');
