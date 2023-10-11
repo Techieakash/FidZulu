a4736bd004b6cb89a12fb723b1267f7aeda6b4e5
Associate <Associate@roifmrwindt>
2023-10-11 14:59:45 +0530

    testing working data phot))

--- spec/jasmine_examples/FoodControllerSpec.js
+++ spec/jasmine_examples/FoodControllerSpec.js
@@ -4,31 +4,27 @@ const bodyParser = require('body-parser');
 const request = require('supertest'); // supertest library for making HTTP requests in tests
 const FoodController = require('../../src/rest-controller/FoodController.js');
 
- 
+
 
 describe('FoodController', () => {
     let app;
 
- 
-
     beforeAll(() => {
         app = express();
         app.use(bodyParser.json());
-
- 
 
         const foodDao = new FoodDao;
         const foodController = new FoodController(foodDao);
         app.get('/food/all/:location', foodController.handleFoodRequest.bind(foodController));
     });
 
- 
+
 
     describe('GET /food/all/:location', () => {
         it('should return all foods for a location', async () => {
             const response = await request(app).get('/food/all/US-NC');
 
- 
+
 
             // Assertions
             expect(response.status).toBe(200);
@@ -36,12 +32,12 @@ describe('FoodController', () => {
             expect(response.body.length).toBeGreaterThan(0);
         });
 
- 
+
 
         it('should handle errors gracefully', async () => {
             const response = await request(app).get('/food/all/Not');
 
- 
+
 
             // Assertions
             expect(response.status).toBe(400);
@@ -54,7 +50,7 @@ describe('FoodController', () => {
                 .get('/food/all/US-NC')
                 .query({ minprice: '10', maxprice: '12', rating: '4', brand: 'FruitFusion' });
 
- 
+
 
             // Assertions
             expect(response.status).toBe(200);
@@ -62,7 +58,7 @@ describe('FoodController', () => {
             expect(response.body.length).toBeGreaterThan(0);
         });
 
- 
+
 
         it('should handle no matching items gracefully', async () => {
             const response = await request(app)
@@ -75,30 +71,30 @@ describe('FoodController', () => {
             expect(response.body.length).toBe(0);
         });
 
- 
+
 
         it('should handle invalid query parameters gracefully', async () => {
             const response = await request(app)
                 .get('/food/all/US-NC')
                 .query({ invalidparam: 'invalidvalue' });
 
- 
+
 
             // Assertions
             expect(response.status).toBe(400); // Bad Request status code
         });
 
         it('should handle invalid  and valid query parameter gracefully', async () => {
-          const response = await request(app)
-              .get('/food/all/US-NC')
-              .query({ invalidparam: 'invalidvalue',minprice:'10' });
-          // Assertions
-          expect(response.status).toBe(200); // Bad Request status code
-      });
+            const response = await request(app)
+                .get('/food/all/US-NC')
+                .query({ invalidparam: 'invalidvalue', minprice: '10' });
+            // Assertions
+            expect(response.status).toBe(200); // Bad Request status code
+        });
 
- 
 
- 
+
+
 
         it('should handle errors gracefully for wrong location', async () => {
             const foodDao = new FoodDao();
@@ -118,7 +114,7 @@ describe('FoodController', () => {
                 .get('/food/all/NOT')
                 .query({ invalid: '10' });
 
- 
+
         });
     });
 });
